c04ceacb5d104ae2700d29176117e856f854c26c
hilmiabdulazisfirmansyah <hilmiabdulazisfirmansyah@gmail.com>
2019-11-07 04:11:12 +0700

    menambahkan autoloader

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -15,4 +15,5 @@ return array(
     'Psr\\Container\\' => array($vendorDir . '/psr/container/src'),
     'Interop\\Container\\' => array($vendorDir . '/container-interop/container-interop/src/Interop/Container'),
     'FastRoute\\' => array($vendorDir . '/nikic/fast-route/src'),
+    'Devisty\\' => array($baseDir . '/app'),
 );
