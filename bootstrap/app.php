b136c4fe04ee470a456e377e330df6331b0412ba
hilmiabdulazisfirmansyah <hilmiabdulazisfirmansyah@gmail.com>
2019-11-08 00:33:44 +0700

    menambahkan fitur eloquent

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,11 +6,30 @@ require __DIR__.'/../vendor/autoload.php';
 $app = new \Slim\App([
 	'settings' => [
 		'displayErrorDetails' => true,
-	]
+		'db' => [
+			'driver' => 'mysql',
+			'host' => 'localhost',
+			'database'	=> 'devisty',
+			'username' => 'root',
+			'password' => '',
+			'charset' => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix' => '',
+		]
+	],
 ]);
 
 $container = $app->getContainer();
 
+// Koneksi Database + membuat eloquent
+$capsule = new \Illuminate\Database\Capsule\Manager;
+$capsule->addConnection($container['settings']['db']);
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
+
+$container['db'] = function ($container) use ($capsule){
+	return $capsule;
+};
 
 $container['view'] = function ($container){
 	$view = new \Slim\Views\Twig(__DIR__.'/../resources/views', [
